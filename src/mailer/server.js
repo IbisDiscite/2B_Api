a9dff7772825a838146aa00ada00ec08c8b78cf2
Pedro Camilo Berrio Pinzon <pcberriop@unal.edu.co>
2018-05-02 18:27:55 -0500

    Update server.js

--- src/mailer/server.js
+++ src/mailer/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.MAILER_URL || 'mailer-ms';
+export const url = process.env.MAILER_URL || 'mailer';
 export const port = process.env.MAILER_PORT || '4003';
 export const entryPoint = process.env.MAILER_ENTRY || 'mailer';
