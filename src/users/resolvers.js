8a0e8936cc3e0143d65db86efd6f2965c63887f1
David Felipe Rodriguez <dafrodriguezro@unal.edu.co>
2018-05-01 23:30:58 -0500

    reparando

--- src/users/resolvers.js
+++ src/users/resolvers.js
@@ -6,7 +6,12 @@ const URL = `http://${url}:${port}/${entryPoint}`;
 const resolvers = {
 	Mutation: {
 		createSession: (_, { session }) =>
-			generalRequest(`${URL}/sign_in`, 'POST', session)
+			generalRequest(`${URL}/sign_in`, 'POST', session).then(
+				(response) => {
+					let user = response.body.data
+					return user
+				}
+			)
 	}
 };
 
