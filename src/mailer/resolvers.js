9f3544172ac796f7c3340131f87cd665e386e34d
Pedro Camilo Berrio Pinzon <pcberriop@unal.edu.co>
2018-05-02 12:37:45 -0500

    mailer

---
+++ src/mailer/resolvers.js
@@ -0,0 +1,23 @@
+import { generalRequest, getRequest } from '../utilities';
+import { url, port, entryPoint } from './server';
+
+const URL = `http://${url}:${port}/${entryPoint}`;
+
+const resolvers = {
+	Query: {
+		allMessage: (_) =>
+			getRequest(URL, ''),
+    messageById: (_, { id }) =>
+			generalRequest(`${URL}/${id}`, 'GET'),
+	},
+	Mutation: {
+		createMessage: (_, { example }) =>
+			generalRequest(`${URL}`, 'POST', message),
+		updateMessage: (_, { id, example }) =>
+			generalRequest(`${URL}/${id}`, 'PUT', message),
+		deleteMessage: (_, { id }) =>
+			generalRequest(`${URL}/${id}`, 'DELETE')
+	}
+};
+
+export default resolvers;
---
+++ src/mailer/server.js
@@ -0,0 +1,3 @@
+export const url = process.env.EXAMPLES_URL || 'mailes-ms';
+export const port = process.env.EXAMPLES_PORT || '4003';
+export const entryPoint = process.env.EXAMPLES_ENTRY || 'mailes';
