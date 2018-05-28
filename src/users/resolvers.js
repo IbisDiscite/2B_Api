79058d511a50798e3acdecf986df62d9173c82c9
David Felipe Rodriguez <dafrodriguezro@unal.edu.co>
2018-05-28 14:30:31 -0500

    :fire: Pruebas cripto

--- src/users/resolvers.js
+++ src/users/resolvers.js
@@ -24,17 +24,21 @@ const resolvers = {
 	},
 
 	Mutation: {
-		createSession: (_, { session }) =>
-			generalRequest(`${URL}/sign_in`, 'POST', session, true).then(
-				(response) => {
-					let user = response.body.data
-					user['token'] = response.headers['access-token']
-					user['uid'] = response.headers['uid']
-					user['type'] = response.headers['token-type']
-					user['client'] = response.headers['client']
-					return user
-				}
-		),
+		createSession: (_, { session }) => {
+			return new Promise((resolve, reject)=>{
+				generalRequest(`${URL}/sign_in`, 'POST', session, true).then(
+					(response) => {
+						console.log("Server response => ", response);
+						let user = response.body.data
+						user['token'] = response.headers['access-token']
+						user['uid'] = response.headers['uid']
+						user['type'] = response.headers['token-type']
+						user['client'] = response.headers['client']
+						resolve(user);
+					}
+				),
+			}
+		}
 	}
 };
 
